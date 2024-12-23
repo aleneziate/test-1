0d86b0646560564580140ec01cba335d4058f522
alene <alene@ABDALLAH>
2024-12-23 11:41:43 +0300

    Add project files.

---
+++ test 1/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+//ques 1 
+//Console.WriteLine("enter number  ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+//for (int i = 0; i <= num; i++)
+//{
+//    sum += i;
+//}
+
+
+
+
+//Console.WriteLine("equal " + sum);
+
+
+
+//question 2 
+//Console.WriteLine("enter a word ");
+//string input = (Console.ReadLine());
+//int vowel = 0;
+//foreach (char A in input)
+//{
+//    if (A == '') 
+//    {
+//        Count++;
+//    }
+    
+//}
+
+//Console.WriteLine("number of vowels is");
+
+
+
+//ques 3
+//Console.WriteLine("enter number  ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int factorial =1;
+//for (int i = 1; i <= num; i++)
+//{
+//    factorial *= i;
+//}
+
+
+
+
+//Console.WriteLine("equal " + factorial);
+
+
+// ques 4  
+
+
+
+//Console.WriteLine("enter number ");
+//int sum = Convert.ToInt32(Console.ReadLine());
+
+//{
+//    if (sum % 2 == 0)
+//    {
+//        Console.WriteLine("True");
+//    }
+//    else
+//    {
+//        Console.WriteLine("False");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
